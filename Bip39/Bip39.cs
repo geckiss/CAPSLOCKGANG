b6f3fd0325c9f69c67d596a889eec6cb48bfd1e9
Henrich Horváth <spixy16@gmail.com>
2019-10-28 00:23:39 +0100

    GenerateSeedAndPhrase & Verify basic implementation

--- Bip39/Bip39.cs
+++ Bip39/Bip39.cs
@@ -34,14 +34,14 @@ namespace CAPSLOCKGANG
             }
 
             // fix wordlist
-            wordList = wordList.Select(line => line.Trim())
-                               .Where(line => line.Length > 0) 
-                               //.OrderBy(line => line)
+            wordList = wordList.Where(line => line != null)
+                               .Select(line => line.Trim())
+                               .Where(line => line.Length > 0)
                                .ToList();
 
             if (wordList.Count != 2048)
             {
-                // TODO: throw exception or?
+                throw new ArgumentException("Line count != 2048", nameof(wordList));
             }
 
             this.wordList = wordList;
@@ -50,9 +50,61 @@ namespace CAPSLOCKGANG
 
         public SeedPhraseResult GenerateSeedAndPhrase(string entropy)
         {
-            throw new NotImplementedException();
+            if (entropy == null)
+            {
+                throw new ArgumentNullException(nameof(entropy));
+            }
+
+            // https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki#generating-the-mnemonic
+            int ENT = entropy.Length;
+            int CS = ENT / 32;
+            int MS = (ENT + CS) / 11;
+
+            var bytes = encoding.GetBytes(entropy);
+            var bitArray = new BitArray(bytes);
+            var words = new List<string>();
+            for (int i = 0; i < bitArray.Count; i += 7)
+            {
+                int index = GetInt(bitArray[i], bitArray[i + 1], bitArray[i + 2], bitArray[i + 3], bitArray[i + 4], bitArray[i + 5], bitArray[i + 6]);
+                words.Add(wordList[index]);
+            }
+            var mnemonicPhrase = string.Join(" ", words);
+
+            if (mnemonicPhrase.Length != MS)
+            {
+                throw new Exception("Invalid result");
+            }
+
+            var seed = "";
+            // TODO https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki#from-mnemonic-to-seed
+
+            return new SeedPhraseResult(mnemonicPhrase, seed);
         }
 
+        private static int GetInt(params bool[] bits)
+        {
+            if (bits.Length != 7)
+            {
+                throw new ArgumentException("Array does not contain 7 bites.", nameof(bits));
+            }
+
+            int GetInt(bool value) => value ? 1 : 0;
+
+            return GetInt(bits[0]) << 6 +
+                   GetInt(bits[1]) << 5 +
+                   GetInt(bits[2]) << 4 +
+                   GetInt(bits[3]) << 3 +
+                   GetInt(bits[4]) << 2 +
+                   GetInt(bits[5]) << 1 +
+                   GetInt(bits[6]);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="mnemonicPhrase">Mnemonic phrase</param>
+        /// <exception cref="NotImplementedException">Not implemented</exception>
+        /// <returns>SeedEntropyResult</returns>
         public SeedEntropyResult GenerateSeedAndEntropy(string mnemonicPhrase)
         {
             throw new NotImplementedException();
@@ -60,9 +112,7 @@ namespace CAPSLOCKGANG
 
         public bool Verify(string mnemonicPhrase, string seed)
         {
-            throw new NotImplementedException();
-
-            // or just: return GenerateSeedAndEntropy(mnemonicPhrase).Seed == seed;
+            return GenerateSeedAndEntropy(mnemonicPhrase).Seed == seed;
         }
     }
 }
